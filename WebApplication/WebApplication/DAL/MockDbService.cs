740477c83313d72f831dacb33ba23890943ace51
Kubion <jakub98kowalski@gmail.com>
2020-03-29 18:04:09 +0200

    Initial

---
+++ WebApplication/WebApplication/DAL/MockDbService.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebApplication.Models;
+
+namespace WebApplication.DAL
+{
+    public class MockDbService : IDbService
+    {
+        private static List<Student> Students = new List<Student>()
+        {
+            new Student()
+            {
+                Id = 1,
+                FirstName = "Jan",
+                LastName = "Kowalski",
+            },
+            new Student()
+            {
+                Id = 2,
+                FirstName = "Anna",
+                LastName = "Malewski",
+            },
+            new Student()
+            {
+                Id = 3,
+                FirstName = "Andrzej",
+                LastName = "Andrzejewicz",
+            }
+        };
+        
+        public IEnumerable<Student> GetStudents()
+        {
+            return Students;
+        }
+    }
+}
